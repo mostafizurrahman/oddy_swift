9e084b0643a055f359f16ab246a4caf7955ba790
Mostafizur Rahman <mostafizur.rahman@surecash.net>
2020-05-08 04:13:33 +0600

    application created

---
+++ oddy/oddy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  oddy
+//
+//  Created by Mostafizur Rahman on 8/5/20.
+//  Copyright © 2020 Mostafizur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ oddy/oddyTests/oddyTests.swift
@@ -0,0 +1,34 @@
+//
+//  oddyTests.swift
+//  oddyTests
+//
+//  Created by Mostafizur Rahman on 8/5/20.
+//  Copyright © 2020 Mostafizur Rahman. All rights reserved.
+//
+
+import XCTest
+@testable import oddy
+
+class oddyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
